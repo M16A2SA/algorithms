a86f54d81bca7ce1b51a8e0f17b67fbd5cb15c97
M16A2SA <Montes.lee@outlook.com>
2022-08-23 13:03:23 +0800

    Update

--- LeetCodeSolution.cs
+++ LeetCodeSolution.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 
 namespace algorithms {
-	public class Solution {
+	public class LeetCodeSolution {
 		public static bool IsPalindrome(int x) {
 			if (x < 0 || (x % 10 == 0 && x != 0)) {
 				return false;
--- Program.cs
+++ Program.cs
@@ -3,12 +3,10 @@
 namespace algorithms {
 	class Program {
 		public static void Main() {
-			int[] test = { 1, 1, 4, 5, 1, 4 };
-			int[] res = Solution.PlusOne(test);
-			foreach (int num in res) {
-				Console.WriteLine(num);
+			int[] numbers = { 1, 2, 3, 4 };
+			foreach (int i in CustomSolution.NonDuplicate(numbers)) {
+				Console.WriteLine(i);
 			}
-			Console.ReadLine();
 		}
 	}
 }
