043e185483635153abf84b925ff2e4fe380778d6
M16A2SA <Montes.lee@outlook.com>
2022-08-23 11:23:54 +0800

    Update

--- Program.cs
+++ Program.cs
@@ -3,7 +3,12 @@
 namespace algorithms {
 	class Program {
 		public static void Main() {
-
+			int[] test = { 1, 1, 4, 5, 1, 4 };
+			int[] res = Solution.PlusOne(test);
+			foreach (int num in res) {
+				Console.WriteLine(num);
+			}
+			Console.ReadLine();
 		}
 	}
 }
